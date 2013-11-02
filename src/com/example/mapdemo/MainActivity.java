d8baa24936678fbf9a358b6e9e9062c393cced67
a-s--k-h <qazsewsxcd@gmail.com>
2013-11-03 02:25:28 +0900

    string.xmlの利用（警告消し）

--- src/com/example/mapdemo/MainActivity.java
+++ src/com/example/mapdemo/MainActivity.java
@@ -10,8 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
