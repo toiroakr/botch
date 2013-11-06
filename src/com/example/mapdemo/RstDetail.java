eb4ab40e03cd4c5f19f53952c31b96ac0a9828d0
petitviolet <violethero0820@gmail.com>
2013-11-06 17:05:50 +0900

    RstDetail.javaで、rst_detail.xmlに表示出来るようになった

--- src/com/example/mapdemo/RstDetail.java
+++ src/com/example/mapdemo/RstDetail.java
@@ -8,12 +8,18 @@ import com.android.volley.Request.Method;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
+import com.example.mapdemo.R.id;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Identity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 
 public class RstDetail extends Activity {
 	private RequestQueue requestQueue;
@@ -48,7 +54,7 @@ public class RstDetail extends Activity {
 						Log.v("error:", error.toString());
 					}
 				});*/
-		
+
 		// マッピング用のRestaurantDetailを作成
 		GsonRequest<RestaurantDetail> req = new GsonRequest<RestaurantDetail>(method, url,
 				RestaurantDetail.class, params, new Listener<RestaurantDetail>() {
@@ -56,8 +62,10 @@ public class RstDetail extends Activity {
 					// 通信成功時のコールバック関数
 					public void onResponse(RestaurantDetail rst) {
 						// success
-						// JsonElement result = rst; // user.get("result").toString();
 						Log.e("success:", rst.toString());
+						setRestaurantDetailToTextView(rst);
+						Log.e("success:", "DONE!");
+						
 					}
 				}, new ErrorListener() {
 					@Override
@@ -72,6 +80,44 @@ public class RstDetail extends Activity {
 		// 通信が終われば、それぞれのreqで定義したコールバック関数が呼ばれる
 		requestQueue.add(req);
     }
-	
+	private void setRestaurantDetailToTextView(RestaurantDetail rst) {
+		TextView name = (TextView) findViewById(id.rst_detail_name);
+		name.setText(rst.getRestaurantName());
+		
+		TextView url = (TextView) findViewById(id.rst_detail_data_tabelog);
+		// urlをclickableにする
+		MovementMethod mMethod = LinkMovementMethod.getInstance();
+		url.setMovementMethod(mMethod);
+		// tabelogmobileurlをhtmlを使ってリンクテキストに埋め込む
+		CharSequence tabelogLink = Html.fromHtml("<a href=\"" + rst.getTabelogMobileUrl() + "\">食べログ</a>");
+		url.setText(tabelogLink);
+		
+		TextView category = (TextView) findViewById(id.rst_detail_data_category);
+		category.setText(rst.getCategory());
+		
+		TextView dinner = (TextView) findViewById(id.rst_detail_data_dinner);
+		dinner.setText(rst.getDinnerPrice());
+		
+		TextView lunch = (TextView) findViewById(id.rst_detail_data_lunch);
+		lunch.setText(rst.getLunchprice());
+		
+		TextView situation = (TextView) findViewById(id.rst_detail_data_situation);
+		situation.setText(rst.getSituation());
+		
+		TextView address = (TextView) findViewById(id.rst_detail_data_address);
+		address.setText(rst.getAddress());
+		
+		TextView station = (TextView) findViewById(id.rst_detail_data_station);
+		station.setText(rst.getStation());
+		
+		TextView tel = (TextView) findViewById(id.rst_detail_data_tel);
+		tel.setText(rst.getTel());
+		
+		TextView bh = (TextView) findViewById(id.rst_detail_data_businesshour);
+		bh.setText(rst.getBusinesshours());
+		
+		TextView holiday = (TextView) findViewById(id.rst_detail_data_holiday);
+		holiday.setText(rst.getHoliday());		
+	}
     
 }
