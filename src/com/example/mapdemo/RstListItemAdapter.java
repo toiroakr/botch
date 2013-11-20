75a45f6c3286e48140c89afd3cab7e4e35d0f670
a-s--k-h <qazsewsxcd@gmail.com>
2013-11-20 21:23:19 +0900

    Hashで出来るようにAdapterを変更

--- src/com/example/mapdemo/RstListItemAdapter.java
+++ src/com/example/mapdemo/RstListItemAdapter.java
@@ -1,6 +1,8 @@
 package com.example.mapdemo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -9,11 +11,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 public class RstListItemAdapter extends ArrayAdapter<Restaurant> {
-
 	private LayoutInflater mLayoutInflater;
+	private ArrayList<Restaurant> rstList = new ArrayList<Restaurant>();
 
-	public RstListItemAdapter(Context context, List<Restaurant> objects) {
-		super(context, 0, objects);
+	public RstListItemAdapter(Context context) {
+		super(context, 0);
 		mLayoutInflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
@@ -40,7 +42,16 @@ public class RstListItemAdapter extends ArrayAdapter<Restaurant> {
 		// 各Viewに表示する情報を設定
 		holder.title.setText(item.getRestaurantName());
 		holder.budget.setText("予算：" + item.getDifficalty());
-		 holder.rate.setRating(position / 6);
+		holder.rate.setRating(position / 6);
 		return view;
+	}
+
+	public void addAll(Map<Integer, Restaurant> rsts) {
+		for (Restaurant rst : rsts.values()) {
+			if (!rstList.contains(rst)) {
+				super.add(rst);
+				rstList.add(rst);
+			}
+		}
 	}
 }
