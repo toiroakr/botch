752fe6c7920ec2d3b0ee71de8261208ab992ad96
a-s--k-h <qazsewsxcd@gmail.com>
2013-11-20 19:11:22 +0900

    Restaurantクラスの変更

--- src/com/example/mapdemo/Restaurant.java
+++ src/com/example/mapdemo/Restaurant.java
@@ -1,34 +1,56 @@
 package com.example.mapdemo;
 
-
 public class Restaurant {
-	int rst_id;
-	String restaurantName;
-	double difficalty;
-	String category;
+	private int rst_id;
+	private String restaurantName;
+	private double difficalty;
+	private String category;
+	private double lon;
+	private double lat;
 
-	public Restaurant(int rst_id, String restaurantName, double diffcalty, String category){
+	public Restaurant(int rst_id, String restaurantName, double diffcalty,
+			String category) {
 		this.rst_id = rst_id;
 		this.restaurantName = restaurantName;
 		this.difficalty = diffcalty;
 		this.category = category;
 	}
 
+	public Restaurant(int rst_id, String restaurantName, double diffcalty,
+			String category, double lon, double lat) {
+		this.rst_id = rst_id;
+		this.restaurantName = restaurantName;
+		this.difficalty = diffcalty;
+		this.category = category;
+		this.lon = lon;
+		this.lat = lat;
+	}
 
-
-	public String getRestaurantName(){
+	public String getRestaurantName() {
 		return restaurantName;
 	}
 
-	public double getDifficalty(){
+	public double getDifficalty() {
 		return difficalty;
 	}
 
-	public String getCategory(){
+	public String getCategory() {
 		return category;
 	}
 
-	public String toString(){
+	public String toString() {
 		return category + " - " + restaurantName;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public int getRst_id() {
+		return rst_id;
 	}
 }
