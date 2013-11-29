a63d1ae0638f14a9a4dcbf39d4a948810b1c1d13
petitviolet <violethero0820@gmail.com>
2013-11-29 14:25:09 +0900

    splash表示された

--- src/kom/botch/rasaki/SplashActivity.java
+++ src/kom/botch/rasaki/SplashActivity.java
@@ -7,20 +7,27 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 
+
 public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splash);
+		/*
+		LinearLayout splashView = (LinearLayout) findViewById(R.layout.splash);
+		ImageView splashImage = new ImageView(this);
+		splashImage.setImageResource(R.id.splash_image);
+		splashView.addView(splashImage);
+		 */
 		Handler hdl = new Handler();
-		hdl.postDelayed(new splashHandler(), 500);
+		hdl.postDelayed(new splashHandler(), 750);
 	}
 
 	class splashHandler implements Runnable {
 		@Override
 		public void run() {
-			Intent i = new Intent(SplashActivity.this.getApplication(),
+			Intent i = new Intent(getApplication(),
 					MainActivity.class);
 			SplashActivity.this.startActivity(i);
 			// SplashActivity.this
